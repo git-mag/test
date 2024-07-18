52cf80f19ba9b18915926bfa0dc3418bbbf21e4c
xs <135936345+git-mag@users.noreply.github.com>
2024-07-18 03:34:28 -0700

    Create more.lua

---
+++ more.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua", false))()
+loadstring(game:HttpGet("https://yarhm.goteamst.com/scr?channel=testing"))()
